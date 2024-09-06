dc4d3ec7471bed563996d2b79b23f0d74c231f38
Vakaar01 <saniabegum904@gmail.com>
2024-09-06 20:35:51 +0530

    Added Palestine flag.py

---
+++ Palestine flag.py
@@ -0,0 +1,78 @@
+#who am i
+from turtle import *
+
+
+#tariwngel
+color('red')
+begin_fill()
+lt(90)
+fd(200)
+rt(135)
+fd(150)
+rt(90)
+fd(150)
+end_fill()
+
+#black tariangel
+
+penup()
+rt(135)
+fd(210)
+pendown()
+rt(90)
+color('black')
+begin_fill()
+fd(300)
+rt(90)
+fd(70)
+rt(90)
+fd(227)
+rt(45)
+fd(100)
+end_fill()
+
+
+
+#white tariangel
+
+penup()
+lt(180)
+fd(100)
+pendown()
+color('white')
+lt(45)
+fd(227)
+rt(90)
+fd(70)
+
+#green reactangl
+
+color('green')
+begin_fill()
+rt(90)
+fd(229)
+lt(45)
+fd(100)
+lt(135)
+fd(300)
+lt(90)
+fd(70)
+end_fill()
+
+#tags
+color('yellow')
+
+
+write("who am i",True,align="right",font=('werdina',15,'bold'))
+
+mainloop()
+
+#who am i
+
+
+
+
+
+
+
+
